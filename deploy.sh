18a977d4ae61cf8958b8e209b99eb8ce4689ecb4
Sean Marquez <capsulecorplab@gmail.com>
2020-03-04 18:13:34 -0800

    patch deploy.sh & .travis.yml
    - set remote to socallinuxexpo/scale-av-web in deploy.sh
    - regenerate secure GH_TOKEN w/ explicit flags as global environment variable, using
      $ travis encrypt GH_TOKEN=<secure_token> --pro --add env.global -r socallinuxexpo/scale-av-web

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ git checkout -b gh-pages
 git add .
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 
-git remote add origin-pages https://$GH_TOKEN@github.com/capsulecorplab/scale-av-web.git > /dev/null 2>&1
+git remote add origin-pages https://$GH_TOKEN@github.com/socallinuxexpo/scale-av-web.git > /dev/null 2>&1
 git push -f origin-pages gh-pages
