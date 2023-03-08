f41c70151e7fa7448879e00a121466f533c8767c
Sean Marquez <capsulecorplab@gmail.com>
2023-03-07 20:05:12 -0700

    replace docker with podman in build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ fi
 
 if ! asciidoctor --version | grep $version > /dev/null; then
     echo "asciidoctor $version not installed"
-    echo "running build via docker..."
-    docker run --rm -v "$PWD:/src" -w "/src" asciidoctor/docker-asciidoctor:1.27.0 asciidoctor index.adoc -o public/index.html
-    docker run --rm -v "$PWD:/src" -w "/src" asciidoctor/docker-asciidoctor:1.27.0 asciidoctor user_manual/index.adoc -o public/scale-av-user-manual.html -acommitHash=$(git rev-parse --short HEAD) -r asciidoctor-diagram
-    docker run --rm -v "$PWD:/src" -w "/src" asciidoctor/docker-asciidoctor:1.27.0 asciidoctor user_manual/index.adoc -o public/scale-av-user-manual.pdf -r asciidoctor-pdf -acommitHash=$(git rev-parse --short HEAD) -r asciidoctor-diagram -b pdf -a pdf-theme=user_manual/theme.yml
+    echo "running build via podman..."
+    podman run --rm -v "$PWD:/src" -w "/src" docker.io/asciidoctor/docker-asciidoctor:1.27.0 asciidoctor index.adoc -o public/index.html
+    podman run --rm -v "$PWD:/src" -w "/src" docker.io/asciidoctor/docker-asciidoctor:1.27.0 asciidoctor user_manual/index.adoc -o public/scale-av-user-manual.html -acommitHash=$(git rev-parse --short HEAD) -r asciidoctor-diagram
+    podman run --rm -v "$PWD:/src" -w "/src" docker.io/asciidoctor/docker-asciidoctor:1.27.0 asciidoctor user_manual/index.adoc -o public/scale-av-user-manual.pdf -r asciidoctor-pdf -acommitHash=$(git rev-parse --short HEAD) -r asciidoctor-diagram -b pdf -a pdf-theme=user_manual/theme.yml
 else
     echo "asciidoctor $version installed!"
     echo "running build..."
